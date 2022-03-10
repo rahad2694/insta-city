0c721eefee8d4f6ecffa48b56c6ff9fe04e3ec16
rahad2694 <rahad.haw@gmail.com>
2022-03-10 22:08:51 +0600

    Report button & div functional, Bug Fixed

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post);
+  // console.log(post);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -153,7 +153,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
